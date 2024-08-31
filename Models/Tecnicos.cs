ebdf12389b7223a7aafe2ee5c1452a4b23ef8978
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2024-08-30 22:55:01 -0400

    Creando modelos.

--- Models/Tecnicos.cs
+++ Models/Tecnicos.cs
@@ -7,7 +7,7 @@ namespace RegistroTecnico.Models
         [Key]
         public int TecId { get; set; }
 
-        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")] 
 
         [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$", ErrorMessage = "El campo solo puede contener letras y espacios.")]
         public string TecNombre { get; set; }
