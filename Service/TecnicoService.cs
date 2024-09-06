419d0fa6341b0f9ec63da275ce7c7d660850fc9e
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2024-09-06 00:25:23 -0400

    Se modifico el nombre de la carpeta y se coloco en plural.

--- Service/TecnicoService.cs
+++ Service/TecnicoService.cs
@@ -3,7 +3,7 @@ using RegistroTecnico.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 
-namespace RegistroTecnico.Service
+namespace REGISTROTECNICO.Service
 {
     public class TecnicoService
     {
