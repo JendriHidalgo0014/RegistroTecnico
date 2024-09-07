2b3fb7f112d22ae702b0d675aba810dba515748e
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2024-09-06 22:00:36 -0400

    Creando las Migraciones.

---
+++ Migrations/20240907015301_Inicial.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroTecnico.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TipoTecnico",
+                columns: table => new
+                {
+                    TipoTecnicoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoTecnico", x => x.TipoTecnicoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tecnicos",
+                columns: table => new
+                {
+                    TecnicoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TecNombre = table.Column<string>(type: "TEXT", nullable: false),
+                    TecSueldoHora = table.Column<decimal>(type: "TEXT", nullable: false),
+                    TipoTecnicoId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tecnicos", x => x.TecnicoId);
+                    table.ForeignKey(
+                        name: "FK_Tecnicos_TipoTecnico_TipoTecnicoId",
+                        column: x => x.TipoTecnicoId,
+                        principalTable: "TipoTecnico",
+                        principalColumn: "TipoTecnicoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tecnicos_TipoTecnicoId",
+                table: "Tecnicos",
+                column: "TipoTecnicoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tecnicos");
+
+            migrationBuilder.DropTable(
+                name: "TipoTecnico");
+        }
+    }
+}
