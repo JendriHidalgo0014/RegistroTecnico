16107ba39e22dd0794b616abe8d1053c5a918dac
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2024-09-06 22:01:59 -0400

    Modificando el service.

--- Service/TecnicoService.cs
+++ Service/TecnicoService.cs
@@ -3,7 +3,7 @@ using RegistroTecnico.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 
-namespace REGISTROTECNICO.Service
+namespace RegistroTecnico.Service
 {
     public class TecnicoService
     {
