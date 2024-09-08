c35e90df04eda11ba7d22916998cb66f348962bd
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2024-09-07 20:30:45 -0400

    Modificando el contexto.

--- DAL/Context.cs
+++ DAL/Context.cs
@@ -3,14 +3,14 @@ using RegistroTecnico.Models;
 
 namespace RegistroTecnico.DAL
 {
-    public class Context : DbContext 
+    public class Context : DbContext
     {
-        public Context(DbContextOptions<Context> options) : base(options) 
+        public Context(DbContextOptions<Context> options) : base(options)
         {
-        
+
         }
 
-        public DbSet<Tecnicos> Tecnicos { get; set; } 
+        public DbSet<Tecnicos> Tecnicos { get; set; }
 
         public DbSet<TipoTecnico> TipoTecnico { get; set; }
 
