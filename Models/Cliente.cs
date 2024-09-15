88f1e1cc45b32ebf9f540403fa6d07fccb58a9c8
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2024-09-14 20:55:27 -0400

    Creando los modelos de cliente y trabajo.

---
+++ Models/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace RegistroTecnico.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int ClienteId { get; set; }
+
+        [Required(ErrorMessage = "El campo Nombres es obligatorio")]
+        public string Nombres { get; set; }
+
+        [Required(ErrorMessage = "Debe colocar un numero de telefono")]
+
+        public int WhatsApp { get; set; }
+
+    }
+}
---
+++ Models/Trabajo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace RegistroTecnico.Models
+{
+    public class Trabajo
+    {
+        [Key]
+
+        public int TrabajoId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int ClienteId { get; set; }
+        public string nombre { get; set; } // Nombre del cliente
+        public int TecnicoId { get; set; }
+        public string TecNombre { get; set; } // Nombre del técnico
+        public string Descripcion { get; set; }
+        public decimal Monto { get; set; }
+
+    }
+}
