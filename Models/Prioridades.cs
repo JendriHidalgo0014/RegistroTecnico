83030da6d01649085623792d0517d8355f9fe259
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2024-09-22 02:27:33 -0400

    Creando el modelo de prioridades y modificando el de trabajo.

---
+++ Models/Prioridades.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnico.Models
+{
+    public class Prioridades
+    {
+        [Key]
+        public int PrioridadId { get; set; }
+
+        [Required(ErrorMessage = "El campo Descripcion es obligatorio")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El campo Tiempo es obligatorio")]
+        public TimeSpan Tiempo { get; set; }  // Usando TimeSpan para representar el tiempo.
+    }
+}
--- Models/Trabajo.cs
+++ Models/Trabajo.cs
@@ -12,9 +12,9 @@ namespace RegistroTecnico.Models
         public int TrabajoId { get; set; }
         public DateTime Fecha { get; set; }
         public int ClienteId { get; set; }
-        public string nombre { get; set; } // Nombre del cliente
+        public string nombre { get; set; } 
         public int TecnicoId { get; set; }
-        public string TecNombre { get; set; } // Nombre del técnico
+        public string TecNombre { get; set; } 
         public string Descripcion { get; set; }
         public decimal Monto { get; set; }
 
